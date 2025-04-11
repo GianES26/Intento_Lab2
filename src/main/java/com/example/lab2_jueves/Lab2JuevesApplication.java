0a77fafc5f20ba796cb83c2824595aa56be4e987
GianES26 <a20224926@pucp.edu.pe>
2025-04-11 16:48:10 -0500

    Primer commit prueba, de Gianfranco btw

---
+++ src/main/java/com/example/lab2_jueves/Lab2JuevesApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab2_jueves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2JuevesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2JuevesApplication.class, args);
+    }
+
+}
